c5e31d174266fef0662098a0e413879a58bf8905
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-08 12:49:38 -0300

    Fix solved

--- icpcarchive.ecs.baylor.edu/ACMContestScoring.cpp
+++ icpcarchive.ecs.baylor.edu/ACMContestScoring.cpp
@@ -5,7 +5,7 @@
  * Date: 07/09/2017
  */
 
- #include <iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -14,7 +14,8 @@ int main(int argc, char const *argv[])
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        int wrong[27];
+        int t[27];
+        bool solved[27];
         int minutes, i, right, total_time;
         char problem;
         int key;
@@ -23,22 +24,25 @@ int main(int argc, char const *argv[])
         while (cin >> minutes) {
                 total_time = right = 0;
                 key = problem - 'A';
-                *wrong = {0};
+                *t = {0};
+                *solved = {false};
 
                 while (minutes != -1) {
                         cin >> problem >> state;
-                        if (state == "wrong") {
-                                wrong[key]++;
-                        } else {
-                                right++;
-                                total_time += minutes;
-                                total_time += wrong[key]*20;
+                        if (!solved[key]) {
+                            if (state[0] == 'r') {
+                                    solved[key] = true;
+                                    right++;
+                                    total_time += minutes + t[key];
+                            } else {
+                                    t[key] += 20;
+                            }
                         }
 
-                        cin >> minutes >> problem >> state;
+                        cin >> minutes;
                 }
 
-                cout << to_string(right) + " " + to_string(total_time) << "\n";
+                cout << right << " " << total_time << endl;
         }
 
         return 0;
