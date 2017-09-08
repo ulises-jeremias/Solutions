d4ec29c057cd9617e58363c6d049a28a0f2d38fa
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-08 00:32:30 -0300

    SBSTR1 solved

---
+++ spoj.com/SBSTR1-SubstringCheck(BugFunny).cpp
@@ -0,0 +1,31 @@
+/*
+ * By: ulises-jeremias
+ * From: http://www.spoj.com/problems/SBSTR1/
+ * Name: SBSTR1 - Substring Check (Bug Funny)
+ * Date: 07/09/2017
+ */
+
+ #include <iostream>
+ #include <string>
+
+ using namespace std;
+
+ static int max_cout = 2;
+
+ int main(int argc, char const *argv[])
+ {
+     string input, pattern, output;
+     int result, i;
+
+     output = "";
+
+     for (i = 0; i < max_cout; i++) {
+         cin >> input >> pattern;
+         result = (int) input.find(pattern) != -1;
+         output += (result ? "1\n" : "0\n");
+     }
+
+     cout << output;
+
+     return 0;
+ }
