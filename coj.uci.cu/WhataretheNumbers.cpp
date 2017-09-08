6ffd76c909cd9295b49d4d396076ee08f185aed1
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-08 00:23:09 -0300

    Fix

--- coj.uci.cu/WhataretheNumbers.cpp
+++ coj.uci.cu/WhataretheNumbers.cpp
@@ -4,3 +4,45 @@
  * Name: What are the Numbers
  * Date: 07/09/2017
  */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    ios_base::sync_with_stdio(false);
+
+    vector<int> inputs;
+    int t, n, i, j, c;
+    bool found;
+
+    inputs.resize(10005);
+    cin >> t;
+
+    for (i = 0; i < t; i++) {
+        found = false;
+        inputs.clear();
+        inputs.assign(10005, 0);
+        cin >> n;
+
+        for (j = 0; j < n; j++) {
+            cin >> c;
+
+            if (inputs[c] == 0) {
+                inputs[c]++;
+            } else {
+                cout << c << endl;
+                if (!found) {
+                  found = true;
+                }
+            }
+        }
+
+        if (!found) {
+          cout << "Not Found!" << endl;
+        }
+    }
+
+    return 0;
+}
