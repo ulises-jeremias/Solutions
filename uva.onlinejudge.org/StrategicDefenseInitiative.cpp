2bae0c96b28db0388162e1dda9a602b626c6c210
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-12 15:14:16 -0300

    Fix

---
+++ uva.onlinejudge.org/StrategicDefenseInitiative.cpp
@@ -0,0 +1,25 @@
+/*
+ * By: ulises-jeremias
+ * From: https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=438
+ * Name: Strategic Defense Initiative
+ * Date: 07/09/2017
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+
+      int n, i;
+
+      while (cin >> n) {
+            for (i = 0; i < n; i++) {
+                  
+            }
+      }
+
+      return 0;
+}
