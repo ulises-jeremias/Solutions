8bb70a0578094ca58259bf9c3c635c114790ce41
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-25 01:28:09 -0300

    ELIS finish

---
+++ spoj.com/ELIS-EasyLongestIncreasingSubsequence.cpp
@@ -0,0 +1,45 @@
+/*
+ * By: ulises-jeremias
+ * From: http://www.spoj.com/problems/ELIS/
+ * Name: ELIS - Easy Longest Increasing Subsequence
+ * Date: 25/09/2017
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+int input[15];
+
+int lis()
+{
+        int i, j, max = -1;
+        int dp[15];
+        for (i = 0; i < n; i++) {
+                dp[i] = 1;
+                for (j = 0; j < i; j++) {
+                        if (input[j] < input[i] && dp[j] + 1 > dp[i]) {
+                                dp[i] = dp[j] + 1;
+                        }
+                }
+                if (dp[i] > max) {
+                        max = dp[i];
+                }
+        }
+        return max;
+}
+
+int main(int argc, char const *argv[])
+{
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int i;
+        cin >> n;
+        for (i = 0; i < n; i++) {
+                cin >> input[i];
+        }
+        cout << lis() << endl;
+        return 0;
+}
