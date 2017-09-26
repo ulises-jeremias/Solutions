ed37eb856f2391364a3121aeaef2ec6c1ef0ae7e
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-25 22:50:59 -0300

    Beautify

--- spoj.com/ELIS-EasyLongestIncreasingSubsequence.java
+++ spoj.com/ELIS-EasyLongestIncreasingSubsequence.java
@@ -10,35 +10,33 @@ import java.math.*;
 import java.util.*;
 
 public class Main {
+        static int n;
+        static int[] inputs = new int[25];
+        static int[] dp = new int[25];
 
-static int n;
-static int[] inputs = new int[25];
-static int[] dp = new int[25];
-
-static int lis () {
-        int i, j, max = -1;
-        for (i = 0; i < n; i++) {
-                dp[i] = 1;
-                for (j = 0; j < i; j++) {
-                        if ((inputs[j] < inputs[i]) && (dp[j]+1 > dp[i])) {
-                                dp[i] = dp[j]+1;
+        static int lis () {
+                int i, j, max = -1;
+                for (i = 0; i < n; i++) {
+                        dp[i] = 1;
+                        for (j = 0; j < i; j++) {
+                                if ((inputs[j] < inputs[i]) && (dp[j]+1 > dp[i])) {
+                                        dp[i] = dp[j]+1;
+                                }
+                        }
+                        if (max < dp[i]) {
+                                max = dp[i];
                         }
                 }
-                if (max < dp[i]) {
-                        max = dp[i];
+                return max;
+        }
+
+        public static void main (String[] args) throws IOException {
+                int i, j;
+                Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+                n = in.nextInt();
+                for (i = 0; i < n; i++) {
+                        inputs[i] = in.nextInt();
                 }
+                System.out.println(lis());
         }
-        return max;
-}
-
-public static void main (String[] args) throws IOException {
-        int i, j;
-        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        n = in.nextInt();
-        for (i = 0; i < n; i++) {
-                inputs[i] = in.nextInt();
-        }
-        System.out.println(lis());
-}
-
 }
