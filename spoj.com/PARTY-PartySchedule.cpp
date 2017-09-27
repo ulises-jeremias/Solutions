b0018b695cc0ff7b50c22ff3a925987e4c39095d
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-27 15:15:54 -0300

    Buffered I/O added

--- spoj.com/PARTY-PartySchedule.cpp
+++ spoj.com/PARTY-PartySchedule.cpp
@@ -37,8 +37,10 @@ pair<int, int> dp(int i, int cap)
 
 int main(int argc, char const *argv[])
 {
-        int i, j;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
 
+        int i, j;
         while (cin >> k >> n, n || k) {
                 for (i = 0; i < n; i++) {
                         cin >> party[i][0] >> party[i][1];
