d72a40c877b64671ee90593f75fec498f5d41d0a
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-06 13:03:14 -0300

    Solved

--- spoj.com/PARTY-PartySchedule.cpp
+++ spoj.com/PARTY-PartySchedule.cpp
@@ -18,20 +18,24 @@ pair<int, int> dp(int i, int cap)
         if (i == n || cap == 0) {
                 return pair<int, int>(0, 0);
         }
+
         if (DP[i][cap].first != -1) {
                 return DP[i][cap];
         }
+
         DP[i][cap] = dp(i + 1, cap);
         if (cap >= party[i][0]) {
                 pair<int, int> a = dp(i + 1, cap - party[i][0]);
                 a.first += party[i][0];
                 a.second += party[i][1];
+
                 if (a.second > DP[i][cap].second) {
                         DP[i][cap] = a;
                 } else if (a.second == DP[i][cap].second) {
                         DP[i][cap].first = min(a.first, DP[i][cap].first);
                 }
         }
+
         return DP[i][cap];
 }
 
@@ -45,11 +49,13 @@ int main(int argc, char const *argv[])
                 for (i = 0; i < n; i++) {
                         cin >> party[i][0] >> party[i][1];
                 }
+
                 for (i = 0; i < n; i++) {
                         for (j = 0; j <= k; j++) {
                                 DP[i][j] = pair<int, int>(-1, -1);
                         }
                 }
+
                 pair<int, int> answer = dp(0, k);
                 cout << answer.first << " " << answer.second << endl;
         }
